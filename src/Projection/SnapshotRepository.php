87e16a363bf8e6d1a157a5ede4b278d05a1a1d74
steph bug <plexikongit@gmail.com>
2021-06-13 14:54:14 +0200

    some work

--- src/Projection/SnapshotRepository.php
+++ src/Projection/SnapshotRepository.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Query\Builder;
 use Chronhub\Snapshot\Store\SnapshotStore;
 use Chronhub\Foundation\Message\DomainEvent;
 use Chronhub\Chronicler\Exception\StreamNotFound;
+use Chronhub\Snapshot\Exception\RuntimeException;
 use Chronhub\Foundation\Support\Contracts\Clock\Clock;
 use Chronhub\Foundation\Support\Contracts\Message\Header;
 use Chronhub\Chronicler\Support\Contracts\Query\QueryFilter;
@@ -19,6 +20,8 @@ use Chronhub\Chronicler\Support\Contracts\Aggregate\AggregateRepositoryWithSnaps
 
 class SnapshotRepository
 {
+    private array $map = [];
+
     public function __construct(private SnapshotStore $snapshotStore,
                                 private AggregateRepositoryWithSnapshotting $aggregateRepository,
                                 private Clock $clock,
@@ -32,13 +35,7 @@ class SnapshotRepository
         $aggregateType = $event->header(Header::AGGREGATE_TYPE);
         $aggregateId = determineAggregateId($event);
 
-        $aggregateRoot = null;
-
-        if (1 === $version) {
-            $aggregateRoot = $this->reconstituteFirstVersion($aggregateId, $aggregateType);
-        } elseif (0 === $version % $this->persisEveryEvents) {
-            $aggregateRoot = $this->reconstituteFromSnapshot($aggregateId, $aggregateType, $version);
-        }
+        $aggregateRoot = $this->reconstituteAggregate($aggregateId, $aggregateType, $version);
 
         if ($aggregateRoot instanceof AggregateRootWithSnapshotting) {
             $snapshot = new Snapshot(
@@ -60,6 +57,67 @@ class SnapshotRepository
     public function deleteAll(string $aggregateType): void
     {
         $this->snapshotStore->deleteAll($aggregateType);
+
+        $this->map = [];
+    }
+
+    /**
+     * Cover scenarios.
+     *
+     *  1) starting snapshot projection along others projections
+     *     snapshot (at least) first version and increment to persist x events
+     *
+     *  2) stopping snapshot projection
+     *     restart again and it will increment normally
+     *
+     *  3) deleting/resetting snapshots projections
+     *     it will start the first version of snapshot at the last event version
+     *
+     *  for 1) and 3)
+     *     it will adjust snapshot version when persist x events match the current event version
+     *     e.g  with a snapshot every 1000 events,
+     *          starting a 3/4016, will take a snapshot at 1000/5000
+     *          and it will increment normally
+     *
+     *  it also cover scenarios when snapshots projections
+     *  and/or snapshots streams positions does/not exists
+     */
+    protected function reconstituteAggregate(AggregateId $aggregateId,
+                                             string $aggregateType,
+                                             int $version): ?AggregateRootWithSnapshotting
+    {
+        if (1 === $version) {
+            return $this->reconstituteFromFirstVersion($aggregateId, $aggregateType);
+        }
+
+        if (isset($this->map[$aggregateId->toString()]) && $version < $this->map[$aggregateId->toString()]) {
+            return null;
+        }
+
+        if (0 === ($version % $this->persisEveryEvents)) {
+            return $this->reconstituteFromSnapshot($aggregateId, $aggregateType, $version);
+        }
+
+        return null;
+    }
+
+    protected function reconstituteFromFirstVersion(AggregateId $aggregateId, string $aggregateType): ?AggregateRootWithSnapshotting
+    {
+        // if snapshots streams positions have been deleted
+        // it will replace current snapshot
+
+        try {
+            $events = $this->fromHistory($aggregateId, 1, PHP_INT_MAX);
+
+            /* @var AggregateRootWithSnapshotting $aggregateType */
+            $aggregateRoot = $aggregateType::reconstituteFromEvents($aggregateId, $events);
+
+            $this->map[$aggregateId->toString()] = $aggregateRoot->version();
+
+            return $aggregateRoot;
+        } catch (StreamNotFound) {
+            throw new RuntimeException('Unable to take first snapshot for aggregate id ' . $aggregateId);
+        }
     }
 
     protected function reconstituteFromSnapshot($aggregateId, $aggregateType, int $toVersion): ?AggregateRootWithSnapshotting
@@ -67,7 +125,9 @@ class SnapshotRepository
         $lastSnapshot = $this->snapshotStore->get($aggregateType, $aggregateId->toString());
 
         if (null === $lastSnapshot) {
-            return null;
+            // happens if snapshots have been deleted, but snapshot stream positions still exists
+            // but it will only be hit when the current event version will match the persist x events
+            return $this->reconstituteFromFirstVersion($aggregateId, $aggregateType);
         }
 
         try {
@@ -80,18 +140,6 @@ class SnapshotRepository
             );
 
             return $aggregateRoot->reconstituteFromSnapshotEvents($events);
-        } catch (StreamNotFound) {
-            return null;
-        }
-    }
-
-    protected function reconstituteFirstVersion(AggregateId $aggregateId, string $aggregateType): ?AggregateRootWithSnapshotting
-    {
-        try {
-            $events = $this->fromHistory($aggregateId, 1, $this->persisEveryEvents);
-
-            /* @var AggregateRootWithSnapshotting $aggregateType */
-            return $aggregateType::reconstituteFromEvents($aggregateId, $events);
         } catch (StreamNotFound) {
             return null;
         }
